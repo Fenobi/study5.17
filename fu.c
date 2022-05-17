5f281a0d37c651eec4f9569007aad9d6e49d511e
Fenobi <13667739122@qq.com>
2022-05-17 16:02:48 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,73 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+////#include <stdio.h>
+////int main()
+////{
+////	int i = 0;
+////	scanf("%d", &i);
+////	printf("%d\n", i);
+////	return 0;
+////}
+//
+//#include <stdio.h>
+//double fenzi(double x, int n);
+//int fenmu(int n);
+//double fun(double x, int n);
+//
+//double fenzi(double x, int n)
+//{
+//	int i;
+//	double s = 1.0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		s = s * x;
+//	}
+//	return s;
+//}
+//int fenmu(int n)
+//{
+//	int i;
+//	int s = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		s *= i;
+//	}
+//	return s;
+//}
+//double fun(double x, int n)
+//{
+//	int i;
+//	double sum = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		sum = sum + (fenzi(x, n) / fenmu(n));
+//	}
+//	return sum;
+//}
+//
+//#include <math.h>
+//
+//int main()
+//{
+//	double x;
+//	int n;
+//	scanf("%d %lf", &n, &x);
+//	printf("sum=%lf\n", fun(x, n));
+//	return 0;
+//	//double a;
+//	//int b;
+//	//printf("%lf\n", a=pow(0.5, 10));
+//	//printf("%d\n", b=fenmu(10));
+//	////int d = (fenzi(0.5, 10) / fenmu(10));
+//	//printf("%lf\n", (double)(a / b));
+//
+//	//return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d", 10 * 10 > 15 ? 10 : 15);
+//	return 0;
+//}
+
